ed9debdf9b8d1b7901281e2a26ceddb7894c9981
Lili <lyly.daire@gmail.com>
2025-05-12 23:12:02 +0200

    fix movement&jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,90 +1,72 @@
-using System.Runtime.CompilerServices;
-using UnityEditor.Rendering;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed;
+    public float jumpForce;
+    private float moveInput;
 
-    public InputActionAsset inputAsset;
-    public float speed = 3;
-    public float sprintSpeed = 6;
-    public float jumpForce = 5f;
+    private Rigidbody2D rb;
 
-    private float _axis;
-    private bool _isUsingSecondary = false;
-    private Rigidbody2D _rigidbody;
+    private bool facingRight = true;
+    
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
 
-    private void OnEnable()
+    private int extraJump;
+    public int extraJumpValue;
+
+    void Start()
     {
-        //inputAsset.FindAction("Gameplay/Jump").started += HandleJump;
-
-        inputAsset.FindAction("Gameplay/Move").performed += HandleMove;
-        inputAsset.FindAction("Gameplay/Move").canceled += HandleMove;
-
-        inputAsset.FindAction("Gameplay/Secondary").started += HandleSecondary;
-        inputAsset.FindAction("Gameplay/Secondary").canceled += HandleSecondary;
-
-        inputAsset.Enable();
+        extraJump = extraJumpValue;
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    private void OnDisable()
+    void FixedUpdate()
     {
-        //inputAsset.FindAction("Gameplay/Jump").started -= HandleJump;
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
 
-        inputAsset.FindAction("Gameplay/Move").performed -= HandleMove;
-        inputAsset.FindAction("Gameplay/Move").canceled -= HandleMove;
+        moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2 (moveInput * speed, rb.velocity.y);
 
-        inputAsset.FindAction("Gameplay/Secondary").started -= HandleSecondary;
-        inputAsset.FindAction("Gameplay/Secondary").canceled -= HandleSecondary;
-
-        inputAsset.Disable();
-
-    }
-    private void Awake()
-    {
-        _rigidbody = GetComponent<Rigidbody2D>();
-    }
-
-    private void Update()
-    {
-        UpdateJump();
-
-        if (_isUsingSecondary)
+        if(facingRight == false && moveInput > 0)
         {
-            transform.position += Vector3.right * _axis * sprintSpeed * Time.deltaTime;
+            Flip();
         }
-        else
+        else if(facingRight == true && moveInput < 0)
         {
-            transform.position += Vector3.right * _axis * speed * Time.deltaTime;
+            Flip();
+        }
+    }
+
+    void Update()
+    {
+        if(isGrounded == true)
+        {
+            extraJump = extraJumpValue;
         }
 
-
-    }
-
-    //private void HandleJump(InputAction.CallbackContext ctx)
-    //{
-    //    Debug.Log($"JUMP: Phase = {ctx.phase} ");
-    //}
-
-    private void HandleMove(InputAction.CallbackContext ctx)
-    {
-        _axis = ctx.ReadValue<float>();
-        Debug.Log($"MOVE: Phase = {ctx.phase} Axis = {_axis}");
-    }
-
-    private void HandleSecondary(InputAction.CallbackContext ctx)
-    {
-        _isUsingSecondary = !ctx.canceled;
-    }
-
-    private void UpdateJump()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.Space) && extraJump > 0)
         {
-            _rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            rb.velocity = Vector2.up * jumpForce;
+            extraJump--;
         }
+        else if(Input.GetKeyDown(KeyCode.Space) && extraJump == 0 && isGrounded == true)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+    }
 
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
     }
 
 }
